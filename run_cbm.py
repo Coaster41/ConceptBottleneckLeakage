dbb70dacb37171b370d03c9ccccf066a6828fe0f
Coen <co41co@gmail.com>
2024-11-16 01:22:46 -0800

    model paramter changes

--- run_cbm.py
+++ run_cbm.py
@@ -16,7 +16,7 @@ sha = repo.git.rev_parse(sha, short=8)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(device)
 
-data_fn = "data/college_acceptance_10001_gaussian.csv"
+data_fn = "data/college_acceptance_100001_gaussian.csv"
 X_headers = ['age', 'sex', 'white', 'asian', 'black', 'hispanic', 'otherRace', 'STEM', 'percentile', 'collegeLevel']
 C_headers = ['gpa', 'SAT', 'SATWriting', 'APTests', 'APScores', 'essays', 'extracurriculars', 'collegeLevel']
 Y_headers = ['accepted']
@@ -28,18 +28,17 @@ latents = 0
 x_to_c_model = XtoCModel(len(X_headers), len(C_headers), latents, final_activation=nn.ReLU()).to(device)
 c_to_y_model = CtoYModel(len(C_headers)+latents, len(Y_headers), final_activation=nn.Sigmoid()).to(device)
 model = FullModel(x_to_c_model, c_to_y_model).to(device)
-print(model)
 
 label_loss = nn.BCELoss()
 learning_rate = 0.001
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 label_loss_weight = 1
-concept_loss_weight = 0.2
-concept_criterion = []
-for _ in range(len(C_headers)):
-    concept_criterion.append(nn.MSELoss())
-# concept_loss_weight = 0.1
-# concept_criterion = nn.MSELoss()
+# concept_loss_weight = 0.2
+# concept_criterion = []
+# for _ in range(len(C_headers)):
+#     concept_criterion.append(nn.MSELoss())
+concept_loss_weight = 0.5
+concept_criterion = nn.MSELoss()
 
 losses = train(model, train_loader, len(C_headers), optimizer, y_criterion=label_loss,
                concept_criterion=concept_criterion, y_weight=label_loss_weight, 
@@ -55,7 +54,7 @@ test_loss = eval(model, test_loader, len(C_headers), y_criterion=label_loss,
                concept_weight=concept_loss_weight, device=device)
 
 plt.plot(range(len(losses)), losses)
-plt.axhline(y = 0.5, color = 'r', linestyle = 'dashed') 
+plt.axhline(y = test_loss, color = 'r', linestyle = 'dashed') 
 plt.legend(['Training Loss', "Test Loss"]) 
 plt.xlabel('Epoch') 
 plt.ylabel('Loss')
